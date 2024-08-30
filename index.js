d68307be54122ee81147a222ecc20288f59ba0dd
HongPhuc2511 <hoanghongphucgl123@gmail.com>
2024-08-30 23:18:49 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-// -------------------------------hiện đăng kí---------------------------------
+
 window.onload=function(){
+// ----------------------------------hiện food------------------------------------
+    let food=document.querySelector("#food")
+    let drink=document.querySelector("#drink")
+let foodclose=document.querySelector("#food-close")
+let foodxacnhan=document.querySelector("#food-xacnhan")
+food.addEventListener("click",function(){
+    document.querySelector(".food-drink").style.display="flex"
+})
+drink.addEventListener("click",function(){
+    document.querySelector(".food-drink").style.display="flex"
+})
+foodclose.addEventListener("click",function(){
+    document.querySelector(".food-drink").style.display="none"
+})
+foodxacnhan.addEventListener("click",function(){
+    document.querySelector(".food-drink").style.display="none"
+})
+// -------------------------------hiện đăng kí---------------------------------
 let addressbtn= document.querySelector('#sign')
 let addressclose=document.querySelector('#addres-close')
 addressbtn.addEventListener("click",function(){
@@ -8,6 +26,29 @@ addressbtn.addEventListener("click",function(){
 addressclose.addEventListener("click",function(){
     document.querySelector(".addres").style.display="none"
 })
+// ---------------------------------------đổi màu ô đặt vé----------------------------------
+// let buttons=document.querySelectorAll('.hang1>button');
+// console.log(buttons)
+// for(let button of buttons){
+//     button.onclick=function(){
+//         if(button.style.backgroundColor==="white"){
+//         button.style.backgroundColor="rgb(227, 39, 165)";
+//     }else{
+//         button.style.backgroundColor="white";
+//     }
+// }
+// }
+// let buttons2=document.querySelectorAll('.hang2');
+// console.log(buttons2)
+// for(let button of buttons2){
+//     button.onclick=function(){
+//         if(button.style.backgroundColor==="white"){
+//         button.style.backgroundColor="rgb(227, 39, 165)";
+//     }else{
+//         button.style.backgroundColor="white";
+//     }
+// }
+// }
 // -----------------------------------hiện đăng nhập--------------------------------
 let addressbtns=document.querySelector('#sign-in')
 let addresscloses=document.querySelector('#address-close')
@@ -17,17 +58,7 @@ addressbtns.addEventListener("click",function(){
 addresscloses.addEventListener("click",function(){
     document.querySelector(".address").style.display="none"
 })
-// ---------------------------------------đổi màu ô đặt vé----------------------------------
-let buttons=document.querySelectorAll('.hang1>button');
-for(let button of buttons){
-    button.onclick=function(){
-        if(button.style.backgroundColor==="white"){
-        button.style.backgroundColor="rgb(227, 39, 165)";
-    }else{
-        button.style.backgroundColor="white";
-    }
-}
-}
+
 // ----------------------------------------hiện đặt vé-------------------------------------
 let datve=document.querySelector('.datve')
 let closechonghe=document.querySelector('#chonghe-close')
@@ -44,7 +75,6 @@ xacnhan.addEventListener("click",function(){
 // ----------------------------------------search----------------------------------------
 let search=document.querySelector('input')
 let movi=document.querySelector('.movi')
-console.log(movi)
     search.onchange=function(){
         let txt=this.value;
         for(let m of movi){
@@ -69,7 +99,6 @@ let phimdangchieu=document.querySelector('.phimdangchieu')
         document.querySelector('.movies').style.display="none"
         document.querySelector('.movies1').style.display="flex"
     })
-// ------------------
 let datve100=document.querySelector('.datve100')
 datve100.addEventListener("click",function(){
     document.querySelector(".chonghe").style.display="flex";
@@ -130,18 +159,18 @@ let datve15=document.querySelector('.datve15')
 datve15.addEventListener("click",function(){
     document.querySelector(".chonghe").style.display="flex";
 })
-// -------------------------------------------hiện check----------------------------------
-let xacnhan1=document.querySelector('#butxacnhan1')
-        let checkclose=document.querySelector('#check-close')
-        xacnhan1.addEventListener("click",function(){
-            document.querySelector('.check-form').style.display="flex"
-        })
-        checkclose.addEventListener("click",function(){
-            document.querySelector('.check-form').style.display="none"
-        })
-
 }
-// -----------------------------------back to top--------------------------
+// -------------------------------------------hiện check----------------------------------
+// let xacnhan1=document.querySelector('#butxacnhan1')
+//         let checkclose=document.querySelector('#check-close')
+//         xacnhan1.addEventListener("click",function(){
+//             document.querySelector('.check-form').style.display="flex"
+//         })
+//         checkclose.addEventListener("click",function(){
+//             document.querySelector('.check-form').style.display="none"
+//         })
+// }
+// -----------------------------------back to top--------------------------------
 $(document).ready(function(){
     $(window).scroll(function(){
         if($(this).scrollTop()>100){
@@ -161,5 +190,40 @@ $(document).ready(function(){
             $('.menucodinh').fadeOut();
         }
     })
+// ---------------------------------------------đổi màu ô phim-----------------------------------
+let buttons = $(".hang1>button");
+buttons.each(function() {
+    $(this).on("click", function() {
+        if ($(this).css('backgroundColor') === 'rgb(255, 255, 255)') {
+            $(this).css('backgroundColor', 'rgb(227, 39, 165)');
+        } else {
+            $(this).css('backgroundColor', 'white');
+        }
+    });
 });
-// -------------------------------------------hiện check----------------------------------
+// -----------------------------------------hiện check----------------------------------
+let xacnhan1=$('#butxacnhan1')
+let checkclose=$('#check-close')
+xacnhan1.on("click",function(){
+    $('.check-form').css('display','flex');
+})
+checkclose.on("click",function(){
+    $('.check-form').css('display','none');
+})
+// ------------------------------------------search-----------------------------------------
+let $search = $('input');
+    let $movi = $('.movi');
+
+    $search.on('input', function() {
+        let txt = $(this).val();
+        $movi.css('border', '').removeClass('hover-effect');
+
+        let $moviname = $('.movi h2 a');
+        $moviname.each(function() {
+            if ($(this).text().includes(txt)) {
+                $(this).closest('.movi').css('border', '5px solid red').addClass('hover-effect');
+            }
+        });
+    });
+});
+ 
